b30085203098b1f085282fa36f39789ec2c5f168
Desmond Edem <edemintech@gmail.com>
2019-09-16 15:18:31 +0100

    chore(setup): setup project architecture

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Welcome = () => <h1>Welcome Book Lovers❤️</h1>
+
+ReactDOM.render(
+  <Welcome />,
+  document.getElementById('index')
+);
